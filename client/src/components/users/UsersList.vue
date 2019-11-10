0b21f8e309026d9500e9b772227371fca1338503
iivanu <ivan.ivanusic@hotmail.com>
2019-11-10 21:23:20 +0100

    Added horizontal line separator

--- client/src/components/users/UsersList.vue
+++ client/src/components/users/UsersList.vue
@@ -2,6 +2,7 @@
     <div class="users-list">
         <p style="font-size:20px">Users: {{users.length}}</p>
         <div align="center" v-for="user in users" v-bind:key="user.id">
+            <hr>
             <UserListItem v-bind:user="user"/>
         </div>
     </div>
@@ -29,7 +30,18 @@ export default {
   float: right;
   margin-right: 50px;
   width: 13rem;
-  height: 20rem;
+  height: 25rem;
   overflow-y: scroll;
+}
+
+hr {
+  width: 100%;
+  height: 2px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 1px;
+  margin-bottom: -4px;
+  background-color:#2c3e50;
+  border: 0 none;
 }
 </style>
