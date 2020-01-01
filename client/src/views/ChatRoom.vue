7829190eaf556fbed1902dc3119bd71bb3ef356f
Mirko Braic <mirko.braic@gmail.com>
2020-01-02 00:54:23 +0100

    Implemented user list and socket rooms

--- client/src/views/ChatRoom.vue
+++ client/src/views/ChatRoom.vue
@@ -28,33 +28,39 @@ export default {
         username: "testUsername",
         color: "#fff1f1"
       },
-      messages: [{username: "Lignja", content: "alo ekipa", timestamp: "10:32", type: 1, color: "#fffff1"}, {username: "Vucica", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", timestamp: "10:33", type: 1, color: "#fff1f1"}, {username: "Mirko", content: "Ullamcorper morbi tincidunt ornare massa. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus. Integer enim neque volutpat ac tincidunt vitae. Penatibus et magnis dis parturient montes nascetur ridiculus mus mauris. Sed viverra ipsum nunc aliquet bibendum. Id venenatis a condimentum vitae sapien. Velit dignissim sodales ut eu sem integer vitae justo eget. Tortor posuere ac ut consequat semper viverra nam libero.", timestamp: "10:34", type: 0, color: "#f1f1ff"}, {username: "gacina matino matan", content: "mi smo braca svi", timestamp: "10:35", type: 1, color: "#f1fff1"}],
-      users: [{username:"Kate"}, {username:"Mirko"}, {username:"Gacelele"}, {username:"Matino"}, {username:"Zubi"}, {username:"Lignja"},{username:"Kate"}, {username:"Mirko"}, {username:"Gacelele"}, {username:"Matino"}, {username:"Zubi"}, {username:"Lignja"},{username:"Kate"}, {username:"Mirko"}, {username:"Gacelele"}, {username:"Matino"}, {username:"Zubi"}, {username:"Lignja"}]
+      room: {
+        name: "world chat",
+        id: "12345"
+      },
+      messages: [],
+      users: []
     };
   },
 
   methods: {
     sendMessage(newMessage) {
       this.messages.push(newMessage);
-      this.$socket.emit('sendMessage', newMessage)
+      this.$socket.emit('sendMessage', newMessage, this.room.id)
     }
   },
 
   sockets: {
     connect() {
-      console.log('user connected') // eslint-disable-line no-console
+      this.$socket.emit('joined', this.room, this.user)
     },
 
     disconnect() {
-      console.log('user disconnected') // eslint-disable-line no-console
+      console.log('disconnected from server') // eslint-disable-line no-console
     },
 
     newMessage(message) {
       this.messages.push(message)
     },
-  },
 
-  created() {}
+    updateUserList(users) {
+      this.users = users
+    }
+  }
 };
 </script>
 
